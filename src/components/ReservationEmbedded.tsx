51a570dd85733a7369f63bb4896bd4fc6df4903b
zahradilpavel-ops <zahradil.pavel@gmail.com>
2025-12-26 12:42:57 +0100

    Update ReservationEmbedded.tsx

--- src/components/ReservationEmbedded.tsx
+++ src/components/ReservationEmbedded.tsx
@@ -1,17 +1,18 @@
+"use client";
+
 import { useEffect } from "react";
 
 const ReservationEmbedded = () => {
   useEffect(() => {
-    // Načtení Previo iframe skriptu – nutné pro funkčnost
     const script = document.createElement("script");
-    script.src = "https://booking.previo.cz/iframe/";
+    script.src = "https://booking.previo.cz/iframe/iframe.js";
     script.type = "text/javascript";
     script.async = true;
     document.body.appendChild(script);
 
     return () => {
       const existingScript = document.querySelector(
-        'script[src="https://booking.previo.cz/iframe/"]'
+        'script[src="https://booking.previo.cz/iframe/iframe.js"]'
       );
       if (existingScript) {
         existingScript.remove();
@@ -20,29 +21,16 @@ const ReservationEmbedded = () => {
   }, []);
 
   return (
-    <section
-      id="reservation"
-      style={{ marginTop: "3rem", textAlign: "center" }}
-    >
-      <h2 style={{ fontSize: "2rem", marginBottom: "2rem" }}>
-        Rezervace
-      </h2>
-
+    <section id="reservation">
       <iframe
         title="Previo Booking System"
         src="https://booking.previo.cz/?hotId=757641&showRoomType=926531&lang=cs"
-        scrolling="no"
-        frameBorder="0"
-        width="100%"
-        height="820"
         id="previo-booking-iframe"
         name="previo-booking-iframe"
-        allowTransparency
-        style={{
-          maxWidth: "100%",
-          display: "block",
-          margin: "0 auto",
-        }}
+        width="100%"
+        height="820"
+        frameBorder="0"
+        scrolling="no"
       />
     </section>
   );
