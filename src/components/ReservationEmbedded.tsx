e9512de5b4fd284e1771397217086bd4b034152b
zahradilpavel-ops <zahradil.pavel@gmail.com>
2025-12-26 13:03:49 +0100

    Update ReservationEmbedded.tsx

--- src/components/ReservationEmbedded.tsx
+++ src/components/ReservationEmbedded.tsx
@@ -1,40 +1,49 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
 const ReservationEmbedded = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://booking.previo.cz/iframe/iframe.js";
-    script.type = "text/javascript";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      const existingScript = document.querySelector(
-        'script[src="https://booking.previo.cz/iframe/iframe.js"]'
-      );
-      if (existingScript) {
-        existingScript.remove();
-      }
-    };
-  }, []);
-
   return (
-    <section id="reservation">
+    <section id="reservation" style={{ marginTop: "3rem" }}>
+      <h2 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        Rezervace
+      </h2>
+
+      {/* Previo script – stejně jako v HTML */}
+      <Script
+        src="https://booking.previo.cz/iframe/"
+        strategy="beforeInteractive"
+      />
+
+      {/* Hlavní iframe */}
       <iframe
-        title="Previo Booking System"
-        src="https://booking.previo.cz/?hotId=757641&showRoomType=952287&lang=cs"
-        id="previo-booking-iframe"
-        name="previo-booking-iframe"
+        src="https://booking.previo.cz/?hotId=757641&showRoomType=926531&lang=cs"
         width="100%"
         height="820"
-        frameBorder="0"
         scrolling="no"
+        allowTransparency
+        title="Previo Booking"
+        style={{
+          border: "none",
+          display: "block",
+          margin: "0 auto",
+          maxWidth: "100%",
+        }}
       />
+
+      {/* Kupony – až PO potvrzení, že funguje hlavní iframe */}
+      {/* 
+      <iframe
+        src="https://booking.previo.cz/?hotId=757641&showTabs=coupon&couponType=cash&lang=cs"
+        width="100%"
+        height="1050"
+        scrolling="no"
+        allowTransparency
+        title="Previo Coupon"
+      />
+      */}
     </section>
   );
 };
 
 export default ReservationEmbedded;
-
