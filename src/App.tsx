625ea6df82887074c17c4911b044594e00eb1ddd
zahradilpavel-ops <zahradil.pavel@gmail.com>
2025-12-26 14:46:17 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/bellevue-mountain-retreat">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
