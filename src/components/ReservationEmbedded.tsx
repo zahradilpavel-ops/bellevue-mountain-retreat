d78f9260057d3e65f1427f5e4f00c7d7f4fbbfec
zahradilpavel-ops <zahradil.pavel@gmail.com>
2025-12-26 12:37:47 +0100

    Update ReservationEmbedded.tsx
    upravený rezervační iframe po vzoru Nováků

--- src/components/ReservationEmbedded.tsx
+++ src/components/ReservationEmbedded.tsx
@@ -1,32 +1,48 @@
-import React from "react";
+import { useEffect } from "react";
 
 const ReservationEmbedded = () => {
-  return (
-    <section id="reservation" style={{ marginTop: "3rem", textAlign: "center" }}>
-      <h2 style={{ fontSize: "2rem", marginBottom: "2rem" }}>Rezervace</h2>
+  useEffect(() => {
+    // Načtení Previo iframe skriptu – nutné pro funkčnost
+    const script = document.createElement("script");
+    script.src = "https://booking.previo.cz/iframe/";
+    script.type = "text/javascript";
+    script.async = true;
+    document.body.appendChild(script);
 
-      {/* Hlavní Previo iframe */}
+    return () => {
+      const existingScript = document.querySelector(
+        'script[src="https://booking.previo.cz/iframe/"]'
+      );
+      if (existingScript) {
+        existingScript.remove();
+      }
+    };
+  }, []);
+
+  return (
+    <section
+      id="reservation"
+      style={{ marginTop: "3rem", textAlign: "center" }}
+    >
+      <h2 style={{ fontSize: "2rem", marginBottom: "2rem" }}>
+        Rezervace
+      </h2>
+
       <iframe
         title="Previo Booking System"
         src="https://booking.previo.cz/?hotId=757641&showRoomType=926531&lang=cs"
+        scrolling="no"
+        frameBorder="0"
         width="100%"
         height="820"
-        frameBorder="0"
-        scrolling="no"
+        id="previo-booking-iframe"
+        name="previo-booking-iframe"
         allowTransparency
-        style={{ maxWidth: "100%", display: "block", margin: "0 auto" }}
-      />
-
-      {/* Doplnkový iframe pro kupony */}
-      <iframe
-        title="Previo Booking Coupon"
-        src="https://booking.previo.cz/?hotId=757641&showTabs=coupon&couponType=cash&lang=cs"
-        width="100%"
-        height="1050"
-        frameBorder="0"
-        scrolling="no"
-        allowTransparency
-        style={{ maxWidth: "100%", display: "block", margin: "2rem auto 0 auto" }}
+        style={{
+          maxWidth: "100%",
+          display: "block",
+          margin: "0 auto",
+        }}
       />
     </section>
   );
